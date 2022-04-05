45e311500123f9fa2d6140bdecfbffa18ace97e1
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-04-05 18:48:52 +0530

    Lost precious Followers but still going

---
+++ Insertion Sort/src/Process/RevStringLeetcode.java
@@ -0,0 +1,30 @@
+package Process;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RevStringLeetcode {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter the size of the char array:");
+        int n = in.nextInt();
+        char[] arr = new char[n];
+        System.out.println("please enter the elements of the char array ");
+        for (int i = 0; i <arr.length ; i++)
+        {
+            arr[i] = in.next().charAt(0);
+        }
+        System.out.println(Arrays.toString(arr));
+        reverseString(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+
+    }
+    static void  reverseString(char[] s)
+    {
+
+        
+
+    }
+}
---
+++ Insertion Sort/src/Process/ReverseString.java
@@ -0,0 +1,7 @@
+package Process;
+// https://practice.geeksforgeeks.org/problems/reverse-a-string/1
+public class ReverseString {
+    public static void main(String[] args) {
+
+    }
+}
